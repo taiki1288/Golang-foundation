098605d427793076722bee8e6232ba18d95fe17e
taiki yamashita <taiki0808ty@gmail.com>
2021-07-17 22:10:16 +0900

    エラー例

--- Greeting.go
+++ Greeting.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 )
 
+// 変数は数字から始めることができない！
+// アンダースコア以外に記号を使うことはできない！
+
 func main() {
 	num := 1
-	fmt.Println(num)
+	num01 := 2
+	num_01 := 3
+	num$01 := 4
+
+	fmt.Println(num$01)
 }
