84fc7b048c9c8fc399b57ee7c872764ec771b013
taiki yamashita <taiki0808ty@gmail.com>
2021-07-17 23:40:13 +0900

    文字列の型

--- Greeting.go
+++ Greeting.go
@@ -6,13 +6,10 @@ import (
 )
 
 func main() {
-	num01 := 123
-	var num02 int = 1234567890
-	num03 := 1.23
-	var num04 float64 = 1.23456789
-
-	fmt.Println(reflect.TypeOf(num01))
-	fmt.Println(reflect.TypeOf(num02))
-	fmt.Println(reflect.TypeOf(num03))
-	fmt.Println(reflect.TypeOf(num04))
+	var string_a string = "Hello,World!"
+	string_b := "Hello,World!"
+	fmt.Println(string_a)
+	fmt.Println(reflect.TypeOf(string_a))
+	fmt.Println(string_b)
+	fmt.Println(reflect.TypeOf(string_b))
 }
