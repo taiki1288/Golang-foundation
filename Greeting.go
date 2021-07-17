78c593c8d654a664dfe172ea5967b1f507670a03
taiki yamashita <taiki0808ty@gmail.com>
2021-07-17 23:44:43 +0900

    bool型文法

--- Greeting.go
+++ Greeting.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var string_a string = "Hello,World!"
-	string_b := "Hello,World!"
-	fmt.Println(string_a)
-	fmt.Println(reflect.TypeOf(string_a))
-	fmt.Println(string_b)
-	fmt.Println(reflect.TypeOf(string_b))
+	a := 10
+	b := 1
+	var num_bool bool = a > b
+
+	fmt.Println(num_bool)
+	fmt.Println(reflect.TypeOf(num_bool))
 }
